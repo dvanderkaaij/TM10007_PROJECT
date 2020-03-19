d6513c8aba358f4ee85fbe0329562f7adabf11d8
javandereijk <j.a.vandereijk@student.tudelft.nl>
2020-03-19 11:31:23 +0100

    Script in alz_assignment

--- alz_assignment.py
+++ alz_assignment.py
@@ -1,1 +1,12 @@
-#test
+'''
+Docstring
+'''
+
+# Data loading functions. Uncomment the one you want to use
+from adni.load_data import load_data
+# from brats.load_data import load_data
+# from hn.load_data import load_data
+
+data = load_data()
+print(f'The number of samples: {len(data.index)}')
+print(f'The number of columns: {len(data.columns)}')
