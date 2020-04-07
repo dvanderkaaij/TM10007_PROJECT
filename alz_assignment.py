76feec6b48b40ae24674dd102e6cdfb34a847f35
javandereijk <j.a.vandereijk@student.tudelft.nl>
2020-04-07 15:53:17 +0200

    Remake classifiers

--- alz_assignment.py
+++ alz_assignment.py
@@ -1,45 +1,44 @@
 '''
-Code for loading the data and split into train and test set.
+This code is a machine learning pipeline for an Alzheimer's disease database
 '''
 
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
 # %%
 # Imports
-from sklearn import model_selection
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
-from adni.load_data import load_data
-from sklearn import metrics
+
+# from sklearn.model_selection import GridSearchCV
+# from missingpy import KNNImputer
+# from sklearn import model_selection
 from sklearn import preprocessing
+from sklearn.svm import SVC
+from sklearn.datasets import load_digits
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import RandomizedSearchCV
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-from missingpy import KNNImputer
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.datasets import load_digits
-from sklearn.model_selection import learning_curve
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.pipeline import Pipeline
+from adni.load_data import load_data
 
 # %%
 # Load data
-DATA_original = load_data()
+DATA_ORIGINAL = load_data()
 DATA = load_data()
 
 # %%
 # Describe data
-AMOUNT_SAMPLES = len(DATA_original.index)
-AMOUNT_FEATURES = len(DATA_original.columns)
+AMOUNT_SAMPLES = len(DATA_ORIGINAL.index)
+AMOUNT_FEATURES = len(DATA_ORIGINAL.columns)
 print(f'The number of samples: {AMOUNT_SAMPLES}')
 print(f'The number of columns: {AMOUNT_FEATURES}')
 
-AMOUNT_AD = sum(DATA_original['label'] == 'AD')
-AMOUNT_CN = sum(DATA_original['label'] == 'CN')
+AMOUNT_AD = sum(DATA_ORIGINAL['label'] == 'AD')
+AMOUNT_CN = sum(DATA_ORIGINAL['label'] == 'CN')
 RATIO_AD = AMOUNT_AD/AMOUNT_SAMPLES
 RATIO_CN = AMOUNT_CN/AMOUNT_SAMPLES
 print(f'The number of AD samples: {AMOUNT_AD} ({round(RATIO_AD*100,2)}%)')
@@ -53,108 +52,148 @@ X = X.drop(['label'], axis=1)
 Y = DATA['label']
 
 # Split dataset --> Trainset(4/5) en Testset(1/5)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.8, stratify=Y)
+X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X, Y, train_size=0.8, stratify=Y)
 
 # Binarize labels in testset
 LB = preprocessing.LabelBinarizer()
-Y_test = LB.fit_transform(Y_test)
+Y_TEST = LB.fit_transform(Y_TEST)
 
 # %% Preprocessing
 
 # Removal of duplicates in X and corresponding Y (1 sample)
-DUPLICATES = X_train[X_train.duplicated(keep='first')]
+DUPLICATES = X_TRAIN[X_TRAIN.duplicated(keep='first')]
 DUPLICATES_ID = DUPLICATES.index
-X_train = X_train.drop(DUPLICATES_ID)
-Y_train = Y_train.drop(DUPLICATES_ID)
+X_TRAIN = X_TRAIN.drop(DUPLICATES_ID)
+Y_TRAIN = Y_TRAIN.drop(DUPLICATES_ID)
 
 # Binarize labels in trainset
 LB = preprocessing.LabelBinarizer()
-Y_train = LB.fit_transform(Y_train)
+Y_TRAIN = LB.fit_transform(Y_TRAIN)
 
 # remove 18 features
-X_train = X_train.T.drop_duplicates().T 
+X_TRAIN = X_TRAIN.T.drop_duplicates().T
 
 # Removal of empty columns
-empty_cols = X_train.columns[(X_train == 0).sum() > 0.8*X_train.shape[0]]
-print(X_train.columns)
-print(f'empty: {empty_cols}') # missing: vf_Frangi_edge_energy_SR(1.0, 10.0)_SS2.0
-X_train = X_train.drop(X_train[empty_cols], axis=1) 
+EMPTY_COLS = X_TRAIN.columns[(X_TRAIN == 0).sum() > 0.8*X_TRAIN.shape[0]]
+print(X_TRAIN.columns)
+print(f'empty: {EMPTY_COLS}')  # missing: vf_Frangi_edge_energy_SR(1.0, 10.0)_SS2.0
+X_TRAIN = X_TRAIN.drop(X_TRAIN[EMPTY_COLS], axis=1)
 
 # Removal of columns with same values
-nunique = X_train.apply(pd.Series.nunique)
-same_cols = nunique[nunique < 3].index
-print(same_cols)
-X_train = X_train.drop(X_train[same_cols], axis=1) # 4 colums removed
+NUNIQUE = X_TRAIN.apply(pd.Series.nunique)
+SAME_COLS = NUNIQUE[NUNIQUE < 3].index
+print(SAME_COLS)
+X_TRAIN = X_TRAIN.drop(X_TRAIN[SAME_COLS], axis=1)  # 4 colums removed
 
 # Scaling: Robust range matching
-scaler = preprocessing.RobustScaler()
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
+SCALER = preprocessing.RobustScaler()
+SCALER.fit(X_TRAIN)
+X_TRAIN = SCALER.transform(X_TRAIN)
 
-# %% KNN
+# Classifiers
+SCORE = {'accuracy': 'accuracy',
+         'roc_auc': 'roc_auc'}
+REFIT = 'roc_auc'
 
-# Initialise lists
-best_n_neighbors = []
-AUC = []
+CV_10 = StratifiedShuffleSplit(n_splits=10, test_size=0.10, train_size=0.90)
 
-# Amount of principal components (0 is without PCA)
-components_list = [1, 2, 3, 4, 5, 10, 20, 50, 100, 0]
+# %% K-Nearest Neighbors (KNN)
 
-for components in components_list:
-    print(f'PCA with {components} components')
-
-    x = X_train
-    if components > 0:
-        p = PCA(n_components=components)
-        p = p.fit(X_train)
-        x = p.transform(X_train)
-
-    knn = KNeighborsClassifier()
-    parameters = {'n_neighbors': list(range(1, 99, 2)),
-                  'weights' : ['uniform', 'distance']}
-
-    cv_10fold = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
-
-    grid_search = model_selection.GridSearchCV(knn, parameters, cv=cv_10fold, scoring='roc_auc')
-    grid_search.fit(x, Y_train)
-
-    clf_knn = grid_search.best_estimator_
-    best_n_neighbors.append(clf_knn.n_neighbors)
-
-    DF = pd.DataFrame(grid_search.cv_results_)
-    AUC.append(max(DF['mean_test_score']))
-
-    print("Best parameters set found on development set:")
-    print(grid_search.best_params_)
-    print(f'Amount of neighbors: {best_n_neighbors[-1]} with AUC: {AUC[-1]}')
-
-# %% RANDOM FOREST
 # Create pipeline a pipeline to search for the best
-# hyperparameters for the combination of PCA and RandomForestClassifier
-
-pipe_rf = Pipeline([('pca', PCA()),
-    ('rf', RandomForestClassifier())])
-score = {'accuracy': 'accuracy'}
+# hyperparameters for the combination of PCA and Nearest Neighbors
+PIPE_KNN = Pipeline([('pca', PCA()),
+                     ('knn', KNeighborsClassifier())])
 
 # The set of hyperparameters to tune
-parameters_rf = {'pca__n_components': [10, 50, 100, 150, 200],
-               'rf__n_estimators': list(range(10, 200, 10)),
-               'rf__max_features': ['auto', 'sqrt'],
-               'rf__max_depth': list(range(10, 50,10)),
-               'rf__min_samples_split': [2, 5, 10],
-               'rf__min_samples_leaf': [1, 2, 4],
-               'rf__bootstrap': [True, False]}     
+PARAMETERS_KNN = {'pca__n_components': [1, 2, 3, 4, 5, 10, 20, 50, 100, 150, 200],
+                  'knn__n_neighbors': list(range(1, 99, 2)),
+                  'knn__weights': ['uniform', 'distance']}
 
-clf_rf_pca = RandomizedSearchCV(pipe_rf, cv=3, n_jobs=-1, n_iter = 100, param_distributions=parameters_rf, scoring=score, refit='accuracy')
+CLF_KNN = RandomizedSearchCV(PIPE_KNN, cv=CV_10, n_jobs=-1, n_iter=100,
+                             param_distributions=PARAMETERS_KNN,
+                             scoring=SCORE, refit=REFIT)
 
-# Train RandomForest classifier
-clf_rf_pca.fit(X_train, Y_train)
+# Train
+CLF_KNN.fit(X_TRAIN, Y_TRAIN)
 
 # DataFrame of the results with the different hyperparameters
-df_results_rf_pca = pd.DataFrame(clf_rf_pca.cv_results_)
+DF_KNN = pd.DataFrame(CLF_KNN.cv_results_)
 
-print("Best parameters set found on development set:")
-print(clf_rf_pca.best_params_)
+print("NN Best parameters set found on development set:")
+print(CLF_KNN.best_params_)
+print(f'Outcome {REFIT}: {CLF_KNN.best_score_}')
+
+CLF_KNN_BEST = CLF_KNN.best_estimator_
+
+# %% Random Forrest (RF)
+# Create pipeline a pipeline to search for the best
+# hyperparameters for the combination of PCA and RandomForestClassifier
+PIPE_RF = Pipeline([('pca', PCA()),
+                    ('rf', RandomForestClassifier())])
+
+# The set of hyperparameters to tune
+PARAMETERS_RF = {'pca__n_components': [1, 2, 3, 4, 5, 10, 20, 50, 100, 150, 200],
+                 'rf__n_estimators': list(range(10, 200, 10)),
+                 'rf__max_features': ['auto', 'sqrt'],
+                 'rf__max_depth': list(range(10, 50, 10)),
+                 'rf__min_samples_split': [2, 5, 10],
+                 'rf__min_samples_leaf': [1, 2, 4],
+                 'rf__bootstrap': [True, False]}
+
+CLF_RF = RandomizedSearchCV(PIPE_RF, cv=CV_10, n_jobs=-1, n_iter=100,
+                            param_distributions=PARAMETERS_RF,
+                            scoring=SCORE, refit=REFIT)
+
+# Train
+CLF_RF.fit(X_TRAIN, Y_TRAIN)
+
+# DataFrame of the results with the different hyperparameters
+DF_RF = pd.DataFrame(CLF_RF.cv_results_)
+
+print("RF Best parameters set found on development set:")
+print(CLF_RF.best_params_)
+print(f'Outcome {REFIT}: {CLF_RF.best_score_}')
+
+CLF_RF_BEST = CLF_RF.best_estimator_
+
+# %% Support Vector Machine (SVM)
+
+# Create pipeline a pipeline to search for the best
+# hyperparameters for the combination of PCA and RandomForestClassifier
+PIPE_SVM = Pipeline([('pca', PCA()),
+                     ('svc', SVC())])
+
+# Parameters:
+# C -> regularization parameter, strength is inversely proportional to C
+# kernel -> linear, poly rbf, sigmoid or precomputed
+# degree -> integer
+# coef0 -> independent term? Only significant in poly and sigmoid
+
+# The set of hyperparameters to tune
+PARAMETERS_SVM = {'pca__n_components': [1, 2, 3, 4, 5, 10, 20, 50, 100, 150, 200],  # Good
+                  'svc__C': [0.01, 0.1, 0.5, 1, 10, 100],
+               #  'svc__gamma': [0.1, 0.01, 0.001, 0.0001, 0.00001],
+                  'svc__gamma': ['scale', 'auto'],
+                  'svc__kernel': ['rbf', 'poly', 'sigmoid'],  # Good
+                  'svc__degree': [2, 4, 6],
+                  'svc__max_iter': [100000]}  # Good
+
+CLF_SVM = RandomizedSearchCV(PIPE_SVM, cv=CV_10, n_jobs=-1, n_iter=100,
+                             param_distributions=PARAMETERS_SVM,
+                             scoring=SCORE, refit=REFIT)
+
+# Train
+CLF_SVM.fit(X_TRAIN, Y_TRAIN)
+
+# DataFrame of the results with the different hyperparameters
+DF_SVM = pd.DataFrame(CLF_SVM.cv_results_)
+
+print("SVM Best parameters set found on development set:")
+print(CLF_SVM.best_params_)
+print(f'Outcome {REFIT}: {CLF_SVM.best_score_}')
+
+CLF_SVM_BEST = CLF_SVM.best_estimator_
+
 
 # %% 
 # LEARNING CURVE, COMPLEXITY
@@ -224,41 +263,3 @@ estimator = clf
 plot_learning_curve(estimator, title, X_train_cv, Y_train_cv, axes=None, ylim=None)
 
 plt.show()
-
-
-# %%
-# Classifiers
-
-# 1. Support Vector Machine 
-
-# Parameters:
-# C -> regularization parameter, strength is inversely proportional to C
-# kernel -> linear, poly rbf, sigmoid or precomputed
-# degree -> integer
-# coef0 -> independent term? Only significant in poly and sigmoid
-
-pipe_svc = Pipeline([('pca', PCA()),
-    ('svc', SVC())])
-score = {'accuracy': 'accuracy'}
-
-hyperparameters = {'pca__n_components': [0, 1, 2, 4, 10, 50, 100, 150, 200],
-                   'svc__C': [0.01, 0.1, 0.5, 1, 10, 100], 
-                   'svc__gamma': [ 0.1, 0.01, 0.001, 0.0001, 0.00001], 
-                   'svc__kernel': ['rbf', 'poly', 'sigmoid'],
-                   'svc__degree': [2,4,6],
-                   'svc__max_iter': [100000]}
-
-clf_svc_pca = RandomizedSearchCV(pipe_svc, cv=10, n_jobs=-1, n_iter= 100, param_distributions=hyperparameters, scoring=score, refit='accuracy')
-
-clf_svc_pca.fit(X_train, Y_train)
-
-# DataFrame of the results with the different hyperparameters
-df_results_svc_pca = pd.DataFrame(clf_svc_pca.cv_results_)
-
-print("Best parameters set found on development set:")
-parameters_svc = clf_svc_pca.best_params_
-print(parameters_svc)
-print('accuracy:')
-print(clf_svc_pca.best_score_)
-
-# %%
